76c2b8d8781fda7d5d823bfad388fdd3a8e2280b
Abiyasa <abiyasa2410@gmail.com>
2020-04-26 10:11:53 +0700

    Fix: change statusCode to code

--- custom.js
+++ custom.js
@@ -1,9 +1,9 @@
 'use strict'
 
-module.exports = function CustomError ({ statusCode = 400, message = 'This is a CustomError', object = {} } = {}) {
+module.exports = function CustomError ({ code = 400, message = 'This is a CustomError', object = {} } = {}) {
   Error.captureStackTrace(this, this.constructor)
   this.name = this.constructor.name
-  this.statusCode = statusCode
+  this.code = code
   this.message = message
   this.object = object
 }
