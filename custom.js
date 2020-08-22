88f57e36e011edc34dceba4fb9c3bd495434f07d
Abiyasa <abiyasa2410@gmail.com>
2020-08-22 12:54:22 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,11 +1,14 @@
 'use strict'
 
-module.exports = function CustomError ({ code = 400, message = 'This is a CustomError', object = {} } = {}) {
-  Error.captureStackTrace(this, this.constructor)
-  this.name = this.constructor.name
-  this.code = code
-  this.message = message
-  this.object = object
+const log = require('debug')('error:custom')
+
+module.exports = function CustomError ({ code = 400, message = 'This is a CustomError', error = {} } = {}) {
+    log('CustomError:', { code, message, error })
+    Error.captureStackTrace(this, this.constructor)
+    this.name = this.constructor.name
+    this.code = code
+    this.message = message
+    this.error = error
 }
 
 require('util').inherits(module.exports, Error)
